9da74c15c46f54b184e444a4fb949d16ae1cf483
Gaurav <gdabli90@gmail.com>
2021-04-22 12:29:00 +0530

    updated comments

--- longestSubstring.py
+++ longestSubstring.py
@@ -1,3 +1,6 @@
+# this program find the longest substring without repeating characters in given string
+# e.g. abcdabcdbb, abcabc, abcefghiefghi
+
 def longestSubstring(input_string):
     maxlenght  = 1
     longest_substring = ''
@@ -5,8 +8,10 @@ def longestSubstring(input_string):
 
     for i in range(N):
         for j in range(i, N):
+            # check if first character matches
             if input_string[i] == input_string[j]:
                 length = len(input_string[i:j])
+                # check if input string and next one is equal 
                 if input_string[i:j] == input_string[j:j+length]:
                     print('till now longest input string is: ', input_string[i:j])
                     print('its length is: ', len(input_string[i:j]))
@@ -19,5 +24,5 @@ def longestSubstring(input_string):
 
 if __name__ == '__main__':
     print('please type input string: ')
-    input_string = input()
+    input_string = input() 
     longestSubstring(input_string)
